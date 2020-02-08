d32ab2e0375a0219144a0bb7fc34ba242b730bb2
thallesBueno <42178992+thallesBueno@users.noreply.github.com>
2020-02-07 21:47:17 -0300

    ajustes heroku

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('/itinerario', async (req,res) => {
     res.send(convertResponse2(data));
 });
 
-const port = 4000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
 });
